7219cbb1678469e1b91a1c04e725999dd00b5941
ghiliweld <ghiliaweld@gmail.com>
2018-09-08 11:58:11 -0400

    Added saveProfile and getHistory to Mimo.js

--- Mimo.js
+++ Mimo.js
@@ -8,10 +8,21 @@ class Mimo {
    * Instantiates Mimo
    *
    * @param     {Web3}      web3          A web3 provider instance
-   * @param     {IPFS}      ipfs          An IPFS instance
+   * @param     {IPFS}      ipfs          An IPFS instance, defaults to Infura node
    * @return    {Mimo}                    self
    */
-  constructor(web3, ipfs) {
+  constructor(
+    web3,
+    ipfs = { host: 'ipfs.infura.io',
+             port: 5001,
+             protocol: 'https',
+             EXPERIMENTAL: {
+               pubsub: true,
+             }
+           }
+  )
+  {
+    if(!web3) throw new Error('Web3 provider not provided')
     OrbitDB.addDatabaseType(MimoStore.type, MimoStore);
     this.orbitdb = new OrbitDB(ipfs);
     this.web3 = web3;
@@ -20,7 +31,7 @@ class Mimo {
   /**
    * Open an existing MimoStore
    *
-   * @param     {String}    name          An ENS name
+   * @param     {String}      name         An ENS name
    * @return    {MimoStore}               The user instance for the given ENS name
    */
   async createProfile(name) {
@@ -33,13 +44,52 @@ class Mimo {
   /**
    * Open an existing MimoStore
    *
-   * @param     {String}    name          An ENS name
+   * @param     {String}      name        An ENS name
    * @return    {MimoStore}               The user instance for the given ENS name
    */
   async openProfile(name) {
     this.web3.eth.ens.getMultihash(name).then(function (hash) {
-      await orbitdb.open('/orbitdb/${hash}/${name}')
+      await orbitdb.open('/orbitdb/${hash}/${name}');
     });
+  }
+
+  /**
+   * Save an existing MimoStore to the ENS resolver
+   *
+   * @param     {OrbitDB}     db           An OrbitDB instance
+   * @param     {Web3Account} account      An Ethereum account
+   */
+  saveProfile(db, account) {
+    let name = db.address.toString().substring(55);
+    let hash = db.address.toString().substring(8, 54);
+    this.web3.eth.ens.setMultihash(name, hash, { from: account });
+  }
+
+  /**
+   * Get all logs in a MimoStore
+   *
+   * @param     {String}     name          An OrbitDB instance
+   * @return    {Array}                    An array of JSON data
+   */
+  async getHistory(name) {
+    if (!(name instanceof String)) throw new Error('name must be a string');
+    await openProfile(name).then(db =>
+      db.iterator()
+      .collect()
+      .map((e) => e.payload.value));
+  }
+
+  /**
+   * Get all logs in a MimoStore
+   *
+   * @param     {OrbitDB}     db           An OrbitDB instance
+   * @return    {Array}                    An array of JSON data
+   */
+  getHistory(db) {
+    if (!(db instanceof OrbitDB)) throw new Error('db must be an OrbitDB instance');
+    db.iterator()
+      .collect()
+      .map((e) => e.payload.value));
   }
 
 }
